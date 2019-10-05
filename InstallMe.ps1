e037f4c2b4fd667ab0642f2929808ff424a1c730
June Castillote <june.castillote@gmail.com>
2019-10-06 03:51:12 +0800

    Update InstallMe.ps1

--- InstallMe.ps1
+++ InstallMe.ps1
@@ -11,7 +11,8 @@ if (!(Test-Path $ModulePath)) {
     New-Item -Path $ModulePath -ItemType Directory -Force | Out-Null
 }
 
+Get-ChildItem -Recurse | Unblock-File
 
 Copy-Item -Path $PSScriptRoot\* -Include *.ps1,*.html,*.psd1,*.psm1 -Destination $ModulePath -Exclude 'installme.ps1' -Force -Confirm:$false
 Copy-Item -Path $PSScriptRoot\Public\* -Destination (New-Item -ItemType Directory $ModulePath\Public -Force).FullName -Force -Confirm:$false
-Copy-Item -Path $PSScriptRoot\Private\* -Destination (New-Item -ItemType Directory $ModulePath\Private -Force).FullName -Force -Confirm:$false
+#Copy-Item -Path $PSScriptRoot\Private\* -Destination (New-Item -ItemType Directory $ModulePath\Private -Force).FullName -Force -Confirm:$false
