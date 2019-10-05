011e7bdb73e513003aa323e98c6495c1e9ca7d93
June Castillote <june.castillote@gmail.com>
2019-10-06 03:20:21 +0800

    Update LazyExchangeAdmin.AdExpNotify.psm1

--- LazyExchangeAdmin.AdExpNotify.psm1
+++ LazyExchangeAdmin.AdExpNotify.psm1
@@ -3,6 +3,6 @@ Get-ChildItem $PSScriptRoot\Public\*.ps1 | ForEach-Object {
     . $_.FullName
 }
 
-Get-ChildItem $PSScriptRoot\Private\*.ps1 | ForEach-Object {
-    . $_.FullName
-}
+# Get-ChildItem $PSScriptRoot\Private\*.ps1 | ForEach-Object {
+#     . $_.FullName
+# }
